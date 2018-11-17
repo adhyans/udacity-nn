9d6c905ce4e8a8e8f106d9354b32ab7e9cabe253
Adhyan <adhyan_srivastava95@yahoo.com>
2018-11-18 00:00:01 +0530

    Added cnn in keras

---
+++ keras-cnn/main.py
@@ -0,0 +1,73 @@
+import keras
+from keras.datasets import cifar10
+import numpy as np
+import matplotlib.pyplot as plt
+from keras.utils import np_utils
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
+from keras.callbacks import ModelCheckpoint # save better model every time encountered
+
+# cifar 10 is a dataset of 60000 tiny images each depicting 1 of 10 classes.
+
+(x_train, y_train), (x_test, y_test) = cifar10.load_data()
+
+fig = plt.figure(figsize=(20,5))
+for i in range(36):
+    ax = fig.add_subplot(3, 12, i + 1, xticks=[], yticks=[])
+    ax.imshow(np.squeeze(x_train[i]))
+
+#rescale 
+x_train = x_train.astype('float32')/255
+x_test = x_test.astype('float32')/255
+
+# one-hot encode the labels
+num_classes = len(np.unique(y_train))
+y_train = keras.utils.to_categorical(y_train, num_classes)
+y_test = keras.utils.to_categorical(y_test, num_classes)
+
+# break training set into training and validation sets
+(x_train, x_valid) = x_train[5000:], x_train[:5000]
+(y_train, y_valid) = y_train[5000:], y_train[:5000]
+
+# print shape of training set
+print('x_train shape:', x_train.shape)
+
+# print number of training, validation, and test images
+print(x_train.shape[0], 'train samples')
+print(x_test.shape[0], 'test samples')
+print(x_valid.shape[0], 'validation samples')
+
+# Model Architecture
+model = Sequential()
+
+model.add(Conv2D(filters=16, kernel_size=2, padding='same', activation='relu', input_shape=(32, 32, 3)))
+model.add(MaxPooling2D(pool_size=2))
+
+model.add(Conv2D(filters=32, kernel_size=2, padding='same', activation='relu'))
+model.add(MaxPooling2D(pool_size=2))
+
+model.add(Conv2D(filters=64, kernel_size=2, padding='same', activation='relu'))
+model.add(MaxPooling2D(pool_size=2))
+model.add(Dropout(0.3))
+
+model.add(Flatten())
+model.add(Dense(500, activation='relu'))
+model.add(Dropout(0.4))
+model.add(Dense(10, activation='softmax'))
+
+model.summary()
+
+# train the model
+checkpointer = ModelCheckpoint(filepath='model.weights.best.hdf5', verbose=1, save_best_only=True)
+
+hist = model.fit(x_train, y_train, batch_size=32, epochs=100,
+          validation_data=(x_valid, y_valid), callbacks=[checkpointer], 
+          verbose=2, shuffle=True)
+
+# load the weights that yielded the best validation accuracy
+model.load_weights('model.weights.best.hdf5')
+
+
+# evaluate and print test accuracy
+score = model.evaluate(x_test, y_test, verbose=0)
+print('\n', 'Test accuracy:', score[1])
